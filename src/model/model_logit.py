d414265958d5658e043c47eab957b3c4902fa964
João Paulo Pizani Flor <paulopizani@posteo.net>
2021-10-26 12:32:45 +0200

    Little refactorings and prettyfying of logit model and test.
    The way this is done will be a template for the other models

--- src/model/model_logit.py
+++ src/model/model_logit.py
@@ -6,13 +6,11 @@
 #
 #  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Tuple
+from scipy.optimize import minimize
+import numpy as np
 
 from model.data_format import ModelArrays
-
-from scipy.optimize import minimize
-
-import numpy as np
 
 
 @dataclass
@@ -43,32 +41,31 @@ class ConfigLogit:
 
 
 class ModelLogit:
-    def __init__(self, params: ConfigLogit, arrays: ModelArrays):
-        self.params = params
+    def __init__(self, cfg: ConfigLogit, arrays: ModelArrays):
+        self.cfg = cfg
         self.arrays = arrays
 
     def run(self):
-
-        # Define seed
-        if self.params.seed:
-            np.random.seed(self.params.seed)
+        # Use passed seed if desired
+        if self.cfg.seed:
+            np.random.seed(self.cfg.seed)
 
         # Prepare data
-        i_obs_y = np.tile(np.random.randint(1,self.arrays.T,size=(self.arrays.NP,1)),(1,self.arrays.T)) == (np.tile(np.arange(1,self.arrays.T + 1),(self.arrays.NP,1)))
+        i_obs_y = \
+            np.tile(np.random.randint(1, self.arrays.T, size=(self.arrays.NP, 1)), (1, self.arrays.T)) \
+            == np.tile(np.arange(1, self.arrays.T + 1), (self.arrays.NP, 1))
         i_obs_x = (i_obs_y == 0)
 
-        BVTT = np.sum(i_obs_y * self.arrays.BVTT,axis=1)
-        sumYBVTT = np.sum(i_obs_x * self.arrays.BVTT*self.arrays.Choice,axis=1)
-        y_regress = np.sum(self.arrays.Choice * i_obs_y,axis=1)
+        BVTT = np.sum(i_obs_y * self.arrays.BVTT, axis=1)
+        sumYBVTT = np.sum(i_obs_x * self.arrays.BVTT * self.arrays.Choice, axis=1)
+        y_regress = np.sum(self.arrays.Choice * i_obs_y, axis=1)
 
         # Starting values and arguments for minimizer
-        x0 = np.array([self.params.mleScale,self.params.mleIntercept,self.params.mleParameter])
-        args = (sumYBVTT,BVTT,y_regress)
-
-        # return ModelLogit.objectiveFunction(x0,sumYBVTT,BVTT,y_regress)
+        x0 = np.array([self.cfg.mleScale, self.cfg.mleIntercept, self.cfg.mleParameter])
+        args = (sumYBVTT, BVTT, y_regress)
 
         # Start minimization routine
-        results = minimize(ModelLogit.objectiveFunction,x0,args=args,method = 'Nelder-Mead')
+        results = minimize(ModelLogit.objectiveFunction, x0, args=args, method='Nelder-Mead')
 
         # Collect results
         x = results['x']
@@ -78,24 +75,19 @@ class ModelLogit:
 
         return x, fval, exitflag, output
 
-
     @staticmethod
-    def objectiveFunction(x,sumYBVTT,BVTT,y_regress):
-
-        # Separate parameters
-        scale = x[0]
-        intercept = x[1]
-        parameter = x[2]
+    def objectiveFunction(x: Tuple[float, float, float], sumYBVTT: np.float64, BVTT: np.float64, y_regress: np.float64):
+        # Separate parameters: x is the estimated (multi-dimensional) parameter
+        scale, intercept, parameter = x
 
         # Create value functions
-        VTT = intercept + parameter*sumYBVTT
-        V1 = scale*BVTT
-        V2 = scale*VTT
+        VTT = intercept + parameter * sumYBVTT
+        V1 = scale * BVTT
+        V2 = scale * VTT
 
         # Create choice probability and Log-likelihood
-        p = np.exp(V1)/(np.exp(V1) + np.exp(V2))
-        ll = - np.sum(np.log(p*(y_regress == 0) + (1-p)*(y_regress==1)))
+        p = np.exp(V1) / (np.exp(V1) + np.exp(V2))
+        ll = - np.sum(np.log(p * (y_regress == 0) + (1 - p) * (y_regress == 1)))
 
         # Return choice probability
         return ll
-
--- src/tests/test_logit.py
+++ src/tests/test_logit.py
@@ -1,31 +1,35 @@
 # Import modules
-from model.data_format import ModelArrays, StudyVar
+import pandas as pd
+from pathlib import Path
+
+from model.data_format import StudyVar
 from model.model_logit import ModelLogit, ConfigLogit
 from model.data_import import make_modelarrays, compute_descriptives
 
-import pandas as pd
 
 # Step 1: read CSV file
-columnarrays = {StudyVar.Id: 'RespID',
-                StudyVar.ChosenAlt: 'Chosen',
-                StudyVar.Cost1: 'CostL',
-                StudyVar.Cost2: 'CostR',
-                StudyVar.Time1: 'TimeL',
-                StudyVar.Time2: 'TimeR',
+columnarrays = {
+    StudyVar.Id: 'RespID',
+    StudyVar.ChosenAlt: 'Chosen',
+    StudyVar.Cost1: 'CostL',
+    StudyVar.Cost2: 'CostR',
+    StudyVar.Time1: 'TimeL',
+    StudyVar.Time2: 'TimeR',
 }
 
-df = pd.read_table('../data/Norway09_data_v5.txt')
+curscript_dir = Path(__file__).resolve().parent
+df = pd.read_table(curscript_dir.parent.parent / 'data' / 'Norway09_data_v5.txt')
 
-model_arrays = make_modelarrays(df,columnarrays)
+model_arrays = make_modelarrays(df, columnarrays)
 
 # Step 2: Do descriptives
 descriptives = compute_descriptives(model_arrays)
 
 # Step 3: Make config
-config = ConfigLogit(1,0.1,1,10000,12345)
+config = ConfigLogit(1, 0.1, 1, 10000, 12345)
 
 # Step 4: Call model
-logit = ModelLogit(config,model_arrays)
+logit = ModelLogit(config, model_arrays)
 
 x, fval, exitflag, output = logit.run()
 
